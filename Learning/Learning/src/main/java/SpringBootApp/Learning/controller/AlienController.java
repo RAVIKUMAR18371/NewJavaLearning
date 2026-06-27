5c0eeaedd6c0a752d560753d190f93e90a5c237e
Ravi Kumar Parasar <ravikumarparasar31@gmail.com>
2026-06-27 18:22:21 +0530

    commit

--- Learning/Learning/src/main/java/SpringBootApp/Learning/controller/AlienController.java
+++ Learning/Learning/src/main/java/SpringBootApp/Learning/controller/AlienController.java
@@ -3,6 +3,7 @@ package SpringBootApp.Learning.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,14 +44,23 @@ public class AlienController {
 
     // Now if i need to update the details inside the Code what should i do next
     @PutMapping("/{id}")
-    public Alien updateAlien(@RequestBody Alien alien,@PathVariable int id, @RequestBody String entity) {
+    public Alien updateAlien(@RequestBody Alien alien, @PathVariable int id) {
         // I am setting the ID for the incoming alien to ensure that i update the correct record
         alien.setId(id);
 
         // In spring Data JPA, .save() will UPDATE the record if the ID already exist in the database
         repo.save(alien);
-        
+
         return alien;
+    }
+
+    // Delete Request (Delete)
+    // This listen for a Delete request at /aliens/{id}
+    @DeleteMapping("/{id}")
+    public String deleteAlien(@PathVariable int id){
+        // SpringData Jpa provides a built in method to delete the ID
+        repo.deleteById(id);
+        return "Deleted alien with Id: " + id;
     }
 
     
